adadae5b9720a6547cfefacf76ecfe63ab235c11
Edward-1020 <iezzuyoung@gmail.com>
2019-06-01 23:16:30 +0800

    feat: data

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { LIST_VIEW, CHART_VIEW, parseToYearAndMonth } from '../utility'
+import { LIST_VIEW, CHART_VIEW, parseToYearAndMonth, padLeft } from '../utility'
 import PriceList from '../components/PriceList';
 import ViewTab from '../components/ViewTab';
 import MonthPicker from '../components/MonthPicker';
@@ -34,7 +34,13 @@ const items = [
       'cid': 2
     }
 ]
-
+const newItem = {
+    'id': 2,
+    'title': '去云南旅游',
+    'price': 300,
+    'date': '2018-09-10',
+    'cid': 2
+}
 class Home extends React.Component {
     constructor (props) {
         super(props);
@@ -45,23 +51,49 @@ class Home extends React.Component {
             tabView: LIST_VIEW
         }
     }
-    changeView = () => {
-
+    changeView = (view) => {
+        this.setState({
+            tabView: view
+        })
     }
-    changeDate = () => {
-
+    changeDate = (year, month) => {
+        this.setState({
+            currentDate: {year, month}
+        });
     }
-    modifyItem = () => {
-
+    modifyItem = (modifyItem) => {
+        const modifiedItems = this.state.items.map((item) => {
+            if (item.id === modifyItem.id) {
+                return {
+                    ...item,
+                    title: '更新后的标题'
+                }
+            } else {
+                return item
+            }
+        });
+        this.setState({
+            items: modifiedItems
+        })
     }
-    deleteItem = () => {
-
+    createItem = () => {
+        this.setState({
+            items: [newItem, ...this.state.items]
+        })
+    }
+    deleteItem = (deletedItem) => {
+        const filteredItems = this.state.items.filter(item => item.id !== deletedItem.id);
+        this.setState({
+            items: filteredItems
+        })
     }
     render () {
         const { items, currentDate, tabView } = this.state;
         const itemsWithCategory = items.map(item => {
             item.category = categories[item.cid];
             return item;
+        }).filter(item => {
+            return item.date.includes(`${currentDate.year}-${padLeft(currentDate.month)}`)
         })
 
         return (
@@ -85,11 +117,14 @@ class Home extends React.Component {
                         activeTab={tabView}
                         onTabChange={this.changeView}
                     />
-                    <PriceList
-                        items={itemsWithCategory}
-                        onModifyItem={this.modifyItem}
-                        onDeleteItem={this.deleteItem}
-                    />
+                    {
+                        tabView === LIST_VIEW ?
+                        <PriceList
+                            items={itemsWithCategory}
+                            onModifyItem={this.modifyItem}
+                            onDeleteItem={this.deleteItem}
+                        /> : <h1>这里是图标区域</h1>
+                    }
                 </div>
             </React.Fragment>
         )
