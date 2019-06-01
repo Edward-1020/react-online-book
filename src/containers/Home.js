73864e4248498ab9d4a591c28c525627529cbb2b
Edward-1020 <iezzuyoung@gmail.com>
2019-06-01 20:41:59 +0800

    feat: state

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,38 +1,58 @@
 import React from 'react';
-import { LIST_VIEW, CHART_VIEW } from '../utility'
+import { LIST_VIEW, CHART_VIEW, parseToYearAndMonth } from '../utility'
 import PriceList from '../components/PriceList';
 import ViewTab from '../components/ViewTab';
 import MonthPicker from '../components/MonthPicker';
 
+const categories = {
+    "1": {
+        'id': '1',
+        'name': '旅行',
+        'type': 'outcome',
+        'iconName': 'ios-plane'
+    },
+    "2": {
+        'id': '1',
+        'name': '旅行',
+        'type': 'outcome',
+        'iconName': 'ios-plane'
+    }
+}
 const items = [
     {
       'id': 1,
       'title': '去云南旅游',
       'price': 200,
       'date': '2018-09-10',
-      'category': {
-        'id': '1',
-        'name': '旅行',
-        'type': 'outcome',
-        'iconName': 'ios-plane'
-      }
+      'cid': 1
     },
     {
       'id': 2,
       'title': '去云南旅游',
       'price': 300,
       'date': '2018-09-10',
-      'category': {
-        'id': '1',
-        'name': '旅行',
-        'type': 'outcome',
-        'iconName': 'ios-plane'
-      }
+      'cid': 2
     }
 ]
 
 class Home extends React.Component {
+    constructor (props) {
+        super(props);
+        this.state = {
+            items,
+            categories,
+            currentDate: parseToYearAndMonth(),
+            tabView: LIST_VIEW
+        }
+    }
+
     render () {
+        const { items, currentDate, tabView } = this.state;
+        const itemsWithCategory = items.map(item => {
+            item.category = categories[item.cid];
+            return item;
+        })
+
         return (
             <React.Fragment>
                 <div className="row mb-5">
@@ -41,8 +61,8 @@ class Home extends React.Component {
                 <div className="row">
                     <div className="col">
                         <MonthPicker
-                            year={2018}
-                            month={8}
+                            year={currentDate.year}
+                            month={currentDate.month}
                             onChange={() => {}}
                         />
                     </div>
@@ -51,11 +71,11 @@ class Home extends React.Component {
                 </div>
                 <div className="content-area py-3 px-3">
                     <ViewTab
-                        activeTab={LIST_VIEW}
+                        activeTab={tabView}
                         onTabChange={() => {}}
                     />
                     <PriceList
-                        items={items}
+                        items={itemsWithCategory}
                         onModifyItem={() => {}}
                         onDeleteItem={() => {}}
                     />
