0091e199d55988111cd26cf71251cdb067ecdfe9
Edward-1020 <iezzuyoung@gmail.com>
2019-06-01 15:26:53 +0800

    feat: PriceList ViewTab

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { LIST_VIEW, CHART_VIEW } from './utility'
+import PriceList from './components/PriceList';
+import ViewTab from './components/ViewTab';
+
+const items = [
+  {
+    'id': 1,
+    'title': '去云南旅游',
+    'price': 200,
+    'date': '2018-09-10',
+    'category': {
+      'id': '1',
+      'name': '旅行',
+      'type': 'outcome',
+      'iconName': 'ios-plane'
+    }
+  },
+  {
+    'id': 2,
+    'title': '去云南旅游',
+    'price': 300,
+    'date': '2018-09-10',
+    'category': {
+      'id': '1',
+      'name': '旅行',
+      'type': 'outcome',
+      'iconName': 'ios-plane'
+    }
+  }
+]
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ViewTab
+        activeTab={LIST_VIEW}
+        onTabChange={(view) => {console.log(view)}}/>
+      <PriceList
+        items={items}
+        onModifyItem={(item) => {alert(item.id)}}
+        onDeleteItem={(item) => {alert(item.id)}}/>
     </div>
   );
 }
